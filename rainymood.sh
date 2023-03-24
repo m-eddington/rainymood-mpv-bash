591e34a196b0480fd5e2551d9c403ff6ad721590
m-eddington <51070722+m-eddington@users.noreply.github.com>
2023-03-24 14:38:53 +0100

    Update rainymood.sh

--- rainymood.sh
+++ rainymood.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #---------------------------------------------------------#
-# http://rainymood.com
+# https://rainymood.com
 #---------------------------------------------------------#
 # Set the playback volume 1-100%
-    VOL=75
+    VOL=80
 
 # Change the links to .mp3 or .m4a if that suits you better
     LINK1="https://rainymood.com/audio1112/0.ogg"
@@ -11,5 +11,7 @@
     LINK3="https://rainymood.com/audio1112/2.ogg"
 
 #---------------------------------------------------------#
-mpv --no-terminal --gapless-audio=yes --volume="$VOL" "$LINK1" "$LINK2" "$LINK3"
+kill_mpv=$( ps -U $USER | grep mpv | head -c 7)
+mpv --no-terminal --gapless-audio=yes --no-ytdl --volume="$VOL" "$LINK1" "$LINK2" "$LINK3"
+killall $kill_mpv
 exit 0
